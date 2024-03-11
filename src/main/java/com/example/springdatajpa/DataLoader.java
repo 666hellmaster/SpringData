39962980327403a058a60b49becab436374276ab
ruzicka <annonymause@email.cz>
2024-03-11 17:17:34 +0100

    nefunkcni ale lepe nefunkcni

--- src/main/java/com/example/springdatajpa/DataLoader.java
+++ src/main/java/com/example/springdatajpa/DataLoader.java
@@ -3,6 +3,7 @@ package com.example.springdatajpa;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
--- src/main/java/com/example/springdatajpa/EmployeeRepository.java
+++ src/main/java/com/example/springdatajpa/EmployeeRepository.java
@@ -1,6 +1,8 @@
 package com.example.springdatajpa;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -9,6 +11,7 @@ import java.util.List;
  * @author ruzicka
  * @since 2024-03-01
  */
+@SuppressWarnings("NullableProblems")
 @Repository
 public interface EmployeeRepository extends JpaRepository<Employee, Long> {
 
