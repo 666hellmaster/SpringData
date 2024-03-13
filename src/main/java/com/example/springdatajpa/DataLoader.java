7ccb9883834dd1249bb3a5b321e8cd142f105d5e
ruzicka <annonymause@email.cz>
2024-03-13 18:16:46 +0100

    prechod na CommandLineRunner

--- src/main/java/com/example/springdatajpa/DataLoader.java
+++ src/main/java/com/example/springdatajpa/DataLoader.java
@@ -5,7 +5,7 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
 import javax.sql.DataSource;
-
+//todo: upravit propojeni s db
 @Component
 public class DataLoader {
 
